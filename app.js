41e08bda9590742e80f04d26549a7a548e0af242
Lucas R. Luque <lucasrluque@icloud.com>
2021-09-29 18:20:54 +0200

    Cosas que me he olvidao de poner jajaja

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import users from './routes/users.js';
+import defaultRoute from './routes/defaultRoute.js';
+ //importamos express y body-parser
+
+const app = express(); //inicializamos express y lo declaramos función "()"
+const PORT = 3000;
+
+
+
+app.use(bodyParser.json());
+app.use('/', defaultRoute);
+app.use('/users', users);
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}/`));
+
